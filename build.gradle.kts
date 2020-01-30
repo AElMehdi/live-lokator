dea4b866d5c419473e8520513b98f60817735506
AElMehdi <assirelmehdi@gmail.com>
2020-01-30 17:32:59 +0100

    Add kafka 2.4 dependency to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 dependencies {
     implementation("org.scala-lang:scala-library:2.12.8")
 
+    implementation("org.apache.kafka:kafka_2.13:2.4.0")
+
     testImplementation("junit:junit:4.12")
     testImplementation("org.scalatest:scalatest_2.12:3.0.8")
 
