da80b446ed44ffb22cb0db9c484ecdf1ef950724
AElMehdi <assirelmehdi@gmail.com>
2020-01-30 17:33:15 +0100

    Write a simple producer class

---
+++ src/main/scala/live/lokator/Producer.scala
@@ -0,0 +1,32 @@
+package live.lokator
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+class Producer {
+
+   def main(args: Array[String]): Unit = {
+      writeToKafka("quick-start")
+   }
+
+   def writeToKafka(topic: String): Unit = {
+      val configuration = configureKafka()
+
+      val producer = new KafkaProducer[String, String](configuration)
+      val record = new ProducerRecord[String, String](topic, "key", "value")
+
+      producer.send(record)
+      producer.close()
+   }
+
+   def configureKafka(): Properties = {
+      val properties = new Properties()
+
+      properties.put("bootstrap.servers", "localhost:9094")
+      properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+      properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+      properties
+   }
+}
