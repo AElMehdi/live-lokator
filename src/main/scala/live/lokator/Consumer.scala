5ea171769444d9996c63440aabd39aa20292e1bf
AElMehdi <assirelmehdi@gmail.com>
2020-02-02 21:06:10 +0100

    Run a Hello World from SBT

--- src/main/scala/live/lokator/Consumer.scala
+++ src/main/scala/live/lokator/Consumer.scala
@@ -1,32 +1,29 @@
 package live.lokator
 
-import java.util.Properties
-
-import org.apache.kafka.clients.consumer.KafkaConsumer
-
 class Consumer {
 
 
   def main(args: Array[String]): Unit = {
-    consumeFromKafka("quick-start")
+    println("Bingo")
+//    consumeFromKafka("quick-start")
   }
-
-  def consumeFromKafka(topic: "quick-start"): Unit = {
-    val properties = new Properties()
-    properties.put("bootstrap.servers", "localhost:9094")
-    properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    properties.put("auto.offset.reset", "latest")
-    properties.put("group.id", "consumer-group")
-
-    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](properties)
 //
-//    consumer.subscribe(util.Arrays.asList(topic))
-//    while (true) {
-//      val record = consumer.poll(Duration.ofMillis(1000))
-//      for (data <- record.iterator) {
-//        println(data)
-//      }
-//    }
-  }
+//  def consumeFromKafka(topic: "quick-start"): Unit = {
+//    val properties = new Properties()
+//    properties.put("bootstrap.servers", "localhost:9094")
+//    properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+//    properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+//    properties.put("auto.offset.reset", "latest")
+//    properties.put("group.id", "consumer-group")
+//
+//    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](properties)
+////
+////    consumer.subscribe(util.Arrays.asList(topic))
+////    while (true) {
+////      val record = consumer.poll(Duration.ofMillis(1000))
+////      for (data <- record.iterator) {
+////        println(data)
+////      }
+////    }
+//  }
 }
---
+++ src/main/scala/live/lokator/Main.scala
@@ -0,0 +1,5 @@
+package live.lokator
+
+class Main {
+
+}
